f3dd2c83f5ef9a7fd2e1463d9c4d3f4cf63a0d57
pyuk <psi.razputin@gmail.com>
2016-09-02 22:25:18 -0500

    more 3

--- 3/lprime.hs
+++ 3/lprime.hs
@@ -1,3 +1,5 @@
+import Data.List ((\\))
+
 testCase :: Integral a => a -> [a] -> Bool
 testCase n [] = True
 testCase n (x:xs)
@@ -10,6 +12,17 @@ prime = p [2..] []
           | testCase x ys = x : p xs (x:ys)
           | otherwise = p xs ys
 
---lPrime :: Integral a => a -> [a]
+prime' :: Integral a => [a]
+prime' = p [2..]
+  where p (x:xs) = x : p [x | x <- (xs `minus` [2*x,3*x..])]
+
+minus :: Ord a => [a] -> [a] -> [a]
+minus xs [] = xs
+minus [] _ = []
+minus (x:xs) (y:ys)
+  | x < y = x : minus xs (y:ys)
+  | x > y = minus (x:xs) ys
+  | otherwise = minus xs ys
+
 lPrime :: Integer -> [Integer]
-lPrime n = [x | x <- (takeWhile (<(n `div` 2)) prime), n `mod` x == 0]
+lPrime n = [x | x <- (takeWhile (<(n `div` 2)) prime'), n `mod` x == 0]
